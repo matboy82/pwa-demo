317607da0cc499b8c81673324748a0e180e16dd4
MatthewBoyer <matthewboyer@truckstop.com>
2021-04-28 13:26:54 -0600

    name change

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('angular-pwa app is running!');
+    expect(await page.getTitleText()).toEqual('pwa-demo app is running!');
   });
 
   afterEach(async () => {
